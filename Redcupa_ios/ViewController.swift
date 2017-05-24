293dcdbbf8599912b856e6010d898d7b4ed4b333
Alonso Martinez <downvec@Alonsos-MacBook-Air.local>
2017-05-24 12:33:52 -0500

    Initial Commit

---
+++ Redcupa_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Redcupa_ios
+//
+//  Created by Alonso Martinez  on 5/24/17.
+//  Copyright © 2017 Redcupa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
