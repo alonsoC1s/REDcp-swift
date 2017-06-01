fac32ee04df0ed13d5ee5ed1b2ac8bc09c0fa98d
Alonso Martinez <downvec@Alonsos-MacBook-Air.local>
2017-06-01 10:44:08 -0500

    kladsjf;a

--- Redcupa_ios/ViewController.swift
+++ Redcupa_ios/ViewController.swift
@@ -7,35 +7,68 @@
 //
 
 import UIKit
+
 import Firebase
+import FirebaseAuthUI
+import FirebaseFacebookAuthUI
+import FBSDKCoreKit
 import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
+    var kFacebookAppID = "PLACE YOUR 16-DIGIT FACEBOOK SECRET HERE (FOUND IN FIREBASE CONSOLE UNDER AUTHENTICATION)"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //FIRApp.configure()
+        checkLoggedIn()
+        
+        
     }
-
-    @IBAction func loginButtonClicked(_ sender: Any) {
-        let facebookLogin = FBSDKLoginManager()
-        facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler: {(FBSDKLoginManagerLoginResult, facebookError) -> Void in
-            if facebookError != nil {
-                print("Facebook Login failed 😕 ")
-            } else if (FBSDKLoginManagerLoginResult!.isCancelled){
-                print("login cncelled")
-            } else{
-                print("youre in 😍")
-                //Trigger Segue
-                
-                
+    
+    func checkLoggedIn() {
+        FIRAuth.auth()?.addStateDidChangeListener { auth, user in
+            if user != nil {
+                // User is signed in.
+            } else {
+                // No user is signed in.
+                self.login()
             }
-        });
-            
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        }
     }
-
+    
+    func login() {
+        let auth = authui
+        let authUI = FIRAuthUI.init(auth: FIRAuth.auth()!)
+        let options = FIRApp.defaultApp()?.options
+        let clientId = options?.clientID
+        let googleProvider = FIRGoogleAuthUI(clientID: clientId!)
+        let facebookProvider = FIRFacebookAuthUI(appID: kFacebookAppID)
+        authUI?.delegate = self
+        authUI?.providers = [googleProvider, facebookProvider]
+        let authViewController = authUI?.authViewController()
+        self.present(authViewController!, animated: true, completion: nil)
+    }
+    
+    @IBAction func logoutUser(_ sender: AnyObject) {
+        try! FIRAuth.auth()!.signOut()
+    }
+    
+    func authUI(_ authUI: FIRAuthUI, didSignInWith user: FIRUser?, error: Error?) {
+        if error != nil {
+            //Problem signing in
+            login()
+        }else {
+            //User is in! Here is where we code after signing in
+            
+        }
+    }
+    
+    func application(app: UIApplication, openURL url: NSURL, options: [String: AnyObject]) -> Bool {
+        let sourceApplication = options[UIApplicationOpenURLOptionUniversalLinksOnly] as! String
+        return FIRAuthUI.default()!.handleOpen(url as URL, sourceApplication: sourceApplication )
+    }
 
 }
 
