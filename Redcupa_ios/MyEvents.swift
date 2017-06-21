89748fec6a77a89f92f25c6392181901a2b74849
downvec <downvec@gmail.com>
2017-06-20 20:26:27 -0500

    k;lajsdf

---
+++ Redcupa_ios/MyEvents.swift
@@ -0,0 +1,44 @@
+//
+//  MyEvents.swift
+//  Redcupa_ios
+//
+//  Created by Alonso Martinez  on 6/20/17.
+//  Copyright © 2017 Redcupa. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseDatabase
+
+class Event: NSObject {
+    public var eventLatitude: Double!
+    public var eventLongitude: Double!
+    public var eventName: String!
+    public var userID: String!
+    public var eventContent: String!
+    public var eventID: String!
+    //public var attendance_list: Dictionary<String, String>
+    //public var invitee_list: Dictionary<String, String>
+    public var eventPublic: Bool!
+    public var eventDate: MyDa te!
+    
+    init(name: String, content: String, userID: String, lat: Double, lng: Double, eventPublic: Bool, eventID: String, year: Int, month: Int, day: Int, hour: Int, minute: Int){
+        self.userID = userID
+        self.eventName = name
+        self.eventContent = content
+        self.eventLatitude = lat
+        self.eventLongitude = lng
+        self.eventPublic = eventPublic
+        self.eventID = eventID
+        
+        self.eventDate = MyDate(year: year,month: month,day: day,hour: hour,minute: minute)
+        
+    }
+    
+    init?(snapshot: FIRDataSnapshot){
+        
+    }
+    
+
+    
+}
